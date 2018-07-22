fd8569e9d22b55eb8e0f58751509f20f484b5e3b
Martin Danielsen <kalroth@gmail.com>
2018-07-22 21:24:56 +0200

    fix: update guessit 1.x code to guessit 2.x code hack: hard-code supported subtitle extensions

--- VideoSort.py
+++ VideoSort.py
@@ -445,15 +445,15 @@ def move_satellites(videofile, dest):
                 # Handle subtitles and nfo files
                 subpart = ''
                 # We support GuessIt supported subtitle extensions
-                if fextlo[1:] in guessit.patterns.extension.subtitle_exts:
-                    guess = guessit.guess_file_info(filename, info=['filename'])
-                    if guess and 'subtitleLanguage' in guess:
+                if fextlo[1:] in ['srt', 'idx', 'sub', 'ssa', 'ass']:
+                    guess = guessit.guessit(filename)
+                    if guess and 'subtitle_language' in guess:
                         fbase = fbase[:fbase.rfind('.')]
                         # Use alpha2 subtitle language from GuessIt (en, es, de, etc.)
-                        subpart = '.' + guess['subtitleLanguage'][0].alpha2
+                        subpart = '.' + guess['subtitle_language'][0].alpha2
                     if verbose:
                         if subpart != '':
-                            print('Satellite: %s is a subtitle [%s]' % (filename, guess['subtitleLanguage'][0]))
+                            print('Satellite: %s is a subtitle [%s]' % (filename, guess['subtitle_language'][0]))
                         else:
                             # English (or undetermined)
                             print('Satellite: %s is a subtitle' % filename)
